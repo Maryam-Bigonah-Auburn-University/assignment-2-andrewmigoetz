e00084908e6894f5bb651727aa41c11278e13d72
andrewmigoetz <93052767+andrewmigoetz@users.noreply.github.com>
2024-09-20 00:02:22 -0500

    Update q2.cpp

--- Demo/q2.cpp
+++ Demo/q2.cpp
@@ -6,43 +6,72 @@ void computeCoin(int coinValue, int& number, int& amountLeft);
 void printOutput(int coinValue, int number);
 
 int main() {
-	char compute_again = 'N';
-	int number = 0,
-	    amountLeft = 0;
+    char compute_again = 'N';
+    int number = 0,
+        amountLeft = 0;
 
-	do {
-		cout << "Enter the amount of change:";
-		cin  >> amountLeft;
-		while (amountLeft < 1 || amountLeft > 99) {
-			cout << "Enter a value between 1 and 99:";
-			cin  >> amountLeft;
-		}
+    do {
+        cout << "Enter the amount of change:"<<endl;
+        cin  >> amountLeft;
+        // Change must be between 1 and 99 inclusive
+        while (amountLeft < 1 || amountLeft > 99) {
+            cout << "Enter a value between 1 and 99:"<<endl;
+            cin  >> amountLeft;
+        }
 
-		cout << "Change can be given as:" << endl;
-		computeCoin(25,number,amountLeft);
-		printOutput(25,number);
-		computeCoin(10,number,amountLeft);
-		printOutput(10,number);
-		computeCoin(1,number,amountLeft);
-		printOutput(1,number);
+        // For each coin calculate how many of each coin to be given and print output
+        cout << "Change can be given as:" << endl;
+        computeCoin(25,number,amountLeft);
+        printOutput(25,number);
+        computeCoin(10,number,amountLeft);
+        printOutput(10,number);
+        computeCoin(1,number,amountLeft);
+        printOutput(1,number);
 
-		cout << "Do you want to make change again?" << endl
-		     << "Enter Y to convert again and any other key to exit:";
-		cin  >> compute_again;
+        // User prompted to enter Y or y to run program again
+        cout << endl << "Do you want to make change again?" << endl
+             << "Enter Y to convert again and any other key to exit:" << endl;
+        cin  >> compute_again;
 
-	} while(compute_again == 'Y' || compute_again == 'y');
+    } while(compute_again == 'Y' || compute_again == 'y');
 
 
-	return 0;
+    return 0;
 }
 
 void computeCoin(int coinValue, int& number, int& amountLeft) {
-	
+        // reset number index for each coin computed
+        number = 0;
+        // amountLeft decreased by coinValue until amountLeft < coinValue
+        // increment number by 1 for each time coinValue fits within amountLeft
+        while (amountLeft > coinValue-1) {
+            amountLeft -= coinValue;
+            number += 1;
+        }
 }
 
 void printOutput(int coinValue, int number) {
-	cout << number;
-	switch(coinValue) {
-		
-	}
+    // print number of coins and convert coinValue into text
+    cout << number;
+    switch(coinValue) {
+        case 25:
+            // consider plural/singular case
+            if (number == 1) {
+                cout << " quarter, ";
+            }
+            else cout << " quarters, ";
+        break;
+        case 10:
+            if (number == 1) {
+                cout << " dime, ";
+            }
+            else cout << " dimes, ";
+        break;
+        case 1:
+            if (number == 1) {
+                cout << " penny";
+            }
+            else cout << " pennies";
+        break;
+    }
 }
