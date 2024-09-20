e5e8e012914e0088717df0ce464da259d56b618f
andrewmigoetz <93052767+andrewmigoetz@users.noreply.github.com>
2024-09-19 23:36:30 -0500

    Added comments to q1.cpp

--- Demo/q1.cpp
+++ Demo/q1.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Fixed lowercase error in "isleapYear"
 bool isLeapYear(int year);
 int getCenturyValue (int year);
 int getYearValue(int year);
@@ -16,6 +17,7 @@ int main() {
         day_of_week = 0;
     string day_string = "";
 
+    // Enter a year value (no conditions on year values specified in assignment)
     cout << "enter the year"<< endl;
 
     cin >> year;
@@ -41,6 +43,7 @@ int main() {
         }
     }
 
+    // Else, if month is February, check if leap year and take in number between 1 and either 28 or 29
     if (month == 2) {
         if (isLeapYear(year)) {
             while (day < 1 || day > 29) {
@@ -56,6 +59,8 @@ int main() {
         }
     }
 
+    // Remaining months must all have 31 days, so else statement covers rest of possibilities
+    
     else {
         while (day < 1 || day > 30) {
             cout << "Enter a value between 1 and 30:"<<endl;
@@ -102,11 +107,13 @@ int main() {
 
 }
 
+// Check if year is leap year or not
 bool isLeapYear(int year) {
     // Leap year is divisible by 400 or divisible by 4 and not 100
     return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0));
 }
 
+// Get the value for century using formula in assignment 2
 int getCenturyValue(int year) {
     int century = 0,
         century_value = 0;
@@ -116,15 +123,18 @@ int getCenturyValue(int year) {
     return century_value;
 }
 
+// Get the value for year using formula in assignment 2
 int getYearValue(int year) {
     int year_value = 0;
-
+    // sum of last two digits (year mod 1000) and last two digits divided by four disregarding remainder (int division disregards remainder inherently)
     year_value = ((year % 1000) / 4)+(year % 1000);
     return year_value;
 }
 
+// Get the value for month using values listed in assignment 2
 int getMonthValue(int month, int year) {
     switch(month) {
+        // months 1 and 2 dependent on leap year
         case 1:
             if (isLeapYear(year))
                 return 6;
@@ -157,7 +167,6 @@ int getMonthValue(int month, int year) {
             return 5;
     }
 }
-
 
 
 
